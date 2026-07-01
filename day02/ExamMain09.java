ca06db2255753a228d7ace14b47097922df728ba
EthanH0428 <johnh0428@gmail.com>
2026-07-01 16:34:54 +0900

    Add files via upload

--- day02/ExamMain09.java
+++ day02/ExamMain09.java
@@ -2,26 +2,33 @@ package kr.ac.kopo.day02;
 
 public class ExamMain09 {
     public static void main(String[] args) {
-        //  1~5줄: 단순히 i개만큼 별 찍기
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
         
-        //  6~9줄 (4줄 분량): 줄이 갈수록 공백이 늘어나고 별은 줄어듦
-        // i는 별의 개수 (4개부터 1개까지 감소)
-        for (int i = 4; i >= 1; i--) {
-            // 공백 출력 (4-i개만큼)
-            for (int j = 1; j <= 4 - i; j++) {
+        // 1줄부터 5줄까지: 별이 1개에서 5개로 늘어남
+        for (int i = 1; i <= 5; i++) {
+            // 1. 공백 출력 (5 - i개)
+            for (int j = 1; j <= 5 - i; j++) {
                 System.out.print(" ");
             }
-            // 별 출력 (i개만큼)
+            // 2. 별 출력 (i개)
             for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println(); // 줄바꿈
         }
+        
+        // 6줄부터 9줄까지: 별이 4개에서 1개로 줄어듦
+        // i는 별의 개수 (4부터 1까지 감소)
+        for (int i = 4; i >= 1; i--) {
+            // 1. 공백 출력 (5 - i개)
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            // 2. 별 출력 (i개)
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
+        
     }
 }
